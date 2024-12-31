bb482b76f240caade715fa2f1101688fc58dec73
OriginalCube <lancemiraflorcube@gmail.com>
2024-12-31 20:35:09 +0800

    feat: added nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/icon', '@pinia/nuxt'],
+    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/icon', '@pinia/nuxt', '@nuxt/fonts'],
     css: ['~/assets/css/main.css'],
 
     imports: { dirs: ['./composables/*/*.{ts,js}'] },
