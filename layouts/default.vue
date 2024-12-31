3750f8e446bbce2c94998d17c3242b18311742a4
OriginalCube <lancemiraflorcube@gmail.com>
2024-12-31 18:36:48 +0800

    chore: added proper file structure for tailwind css & updated comments

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-screen items-center justify-center bg-slate-800">
+    <div class="flex min-h-screen items-center justify-center">
         <slot />
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,16 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    imports: { dirs: ['./composables/*/*.{ts,js}'] },
     modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/icon', '@pinia/nuxt'],
-    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
 
+    imports: { dirs: ['./composables/*/*.{ts,js}'] },
+    devtools: { enabled: true },
     pinia: {
         storesDirs: ['./stores/**', './custom-folder/stores/**'],
     },
 
     runtimeConfig: {
-        //env: process.env.NODE_ENV,
+        //env: import.meta.env.ENVIRONMENT,
     },
 })
